b1e70aeacbf30f5bb63a8c67b492cebcd3593b24
Peter Mitrano <pmitrano@armstorm>
2021-02-07 12:29:11 -0500

    more testing of cdcpd2 with obstacle constraints

--- link_bot_gazebo_python/scripts/val_grasp_rope.py
+++ link_bot_gazebo_python/scripts/val_grasp_rope.py
@@ -11,8 +11,8 @@ def main():
     s.robot.plan_to_joint_config('both_arms', 'home')
     s.open_grippers_if_not_grasping()
     s.grasp_rope_endpoints()
-    s.robot.open_left_gripper()
-    s.robot.open_right_gripper()
+    # s.robot.open_left_gripper()
+    # s.robot.open_right_gripper()
 
 
 if __name__ == "__main__":
