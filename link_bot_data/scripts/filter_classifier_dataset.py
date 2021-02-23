7d9b1993afb6dc848cef917ccde7d1b5b8660ea3
Peter Mitrano <pmitrano@armstorm>
2021-02-23 15:42:12 -0500

    filter dataset script

---
+++ link_bot_data/scripts/filter_classifier_dataset.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import argparse
+import pathlib
+from typing import Dict
+
+import colorama
+
+import rospy
+from link_bot_data.classifier_dataset import ClassifierDatasetLoader
+from link_bot_data.modify_dataset import modify_dataset
+from link_bot_pycommon.args import my_formatter
+
+
+def main():
+    colorama.init(autoreset=True)
+
+    parser = argparse.ArgumentParser(formatter_class=my_formatter)
+    parser.add_argument('dataset_dir', type=pathlib.Path, help='dataset directory')
+    parser.add_argument('suffix', type=str, help='string added to the new dataset name')
+
+    args = parser.parse_args()
+
+    rospy.init_node("modify_dynamics_dataset")
+
+    outdir = args.dataset_dir.parent / f"{args.dataset_dir.name}+{args.suffix}"
+
+    def _process_example(dataset: ClassifierDatasetLoader, example: Dict):
+        example['prediction_start_t'] = 0
+        yield example
+
+    hparams_update = {}
+
+    dataset = ClassifierDatasetLoader([args.dataset_dir], use_gt_rope=False, load_true_states=True)
+    modify_dataset(dataset_dir=args.dataset_dir,
+                   dataset=dataset,
+                   outdir=outdir,
+                   process_example=_process_example,
+                   hparams_update=hparams_update)
+
+
+if __name__ == '__main__':
+    main()
