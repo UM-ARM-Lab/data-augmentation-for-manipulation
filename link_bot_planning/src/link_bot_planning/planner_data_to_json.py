fade7c41457cc2d21acbd2dccf996a5d12250d1f
Peter Mitrano <pmitrano@armstorm>
2021-03-13 23:24:29 -0500

    initial testing of extracting the controls from the planning tree

--- link_bot_planning/src/link_bot_planning/planner_data_to_json.py
+++ link_bot_planning/src/link_bot_planning/planner_data_to_json.py
@@ -1,11 +1,12 @@
 from ompl import base as ob
+from ompl import control as oc
 
 from link_bot_pycommon.scenario_ompl import ScenarioOmpl
 from moonshine.moonshine_utils import listify
 
 
-def planner_data_to_json(planner_data, scenario_ompl: ScenarioOmpl):
-    json = {
+def planner_data_to_json(planner_data: oc.PlannerData, scenario_ompl: ScenarioOmpl):
+    tree = {
         'vertices': [],
         'edges':    [],
     }
@@ -20,15 +21,19 @@ def planner_data_to_json(planner_data, scenario_ompl: ScenarioOmpl):
         edges_map = ob.mapUintToPlannerDataEdge()
 
         np_s = scenario_ompl.ompl_state_to_numpy(s)
-        json['vertices'].append(listify(np_s))
+        tree['vertices'].append(listify(np_s))
 
         planner_data.getEdges(vertex_index, edges_map)
-        for vertex_index2 in edges_map.keys():
+        for vertex_index2, control in edges_map:
+            control_duration = control.getDuration()
             v2 = planner_data.getVertex(vertex_index2)
             s2 = v2.getState()
+            control_np = scenario_ompl.ompl_control_to_numpy(s2, control.getControl())
             np_s2 = scenario_ompl.ompl_state_to_numpy(s2)
-            json['edges'].append(listify({
-                'from': np_s,
-                'to':   np_s2,
+            tree['edges'].append(listify({
+                'from':     np_s,
+                'action':   control_np,
+                'duration': control_duration,
+                'to':       np_s2,
             }))
-    return json
+    return tree
