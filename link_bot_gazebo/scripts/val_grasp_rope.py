96f4d39519a299000cc937ffde1e10b2dcd7d69b
Peter Mitrano <pmitrano@armstorm>
2021-03-13 21:46:02 -0500

    new scenes and goals

--- link_bot_gazebo/scripts/val_grasp_rope.py
+++ link_bot_gazebo/scripts/val_grasp_rope.py
@@ -8,7 +8,7 @@ def main():
     s = SimDualArmRopeScenario('hdt_michigan')
     s.on_before_get_state_or_execute_action()
 
-    s.robot.plan_to_joint_config('both_arms', 'home')
+    # s.robot.plan_to_joint_config('both_arms', 'home')
     s.open_grippers_if_not_grasping()
     s.grasp_rope_endpoints()
 
