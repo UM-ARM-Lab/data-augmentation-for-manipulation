d2902cef8d444ce6c3a3a668efddf627a82a4eef
Peter <peter@armnova>
2021-03-16 18:45:37 -0400

    new way to pick filter funcs for filter_classifier_dataset
    add configs for running fine-tuned models in the rack env

--- link_bot_data/scripts/filter_classifier_dataset.py
+++ link_bot_data/scripts/filter_classifier_dataset.py
@@ -2,6 +2,7 @@
 import argparse
 import pathlib
 from typing import Dict
+import inspect
 
 import colorama
 
@@ -11,11 +12,22 @@ from link_bot_data.modify_dataset import modify_dataset, filter_dataset
 from link_bot_pycommon.args import my_formatter
 
 
+def starts_close(dataset: ClassifierDatasetLoader, example: Dict):
+    starts_close = (example['is_close'][0] == 1)
+    return starts_close
+
+def starts_far(dataset: ClassifierDatasetLoader, example: Dict):
+    starts_far = (example['is_far'][0] == 0)
+    return starts_far
+
+filter_funcs = {name: f for name, f in vars().items() if inspect.isfunction(f)}
+
 def main():
     colorama.init(autoreset=True)
 
     parser = argparse.ArgumentParser(formatter_class=my_formatter)
     parser.add_argument('dataset_dir', type=pathlib.Path, help='dataset directory')
+    parser.add_argument('filter_func_name', type=str, help='name of one of the above free functions for filtering')
     parser.add_argument('suffix', type=str, help='string added to the new dataset name')
 
     args = parser.parse_args()
@@ -24,17 +36,23 @@ def main():
 
     outdir = args.dataset_dir.parent / f"{args.dataset_dir.name}+{args.suffix}"
 
-    def _should_keep_example(dataset: ClassifierDatasetLoader, example: Dict):
-        starts_close = (example['is_close'][0] == 1)
-        return starts_close
-
     hparams_update = {}
+
+
+    if args.filter_func_name in filter_funcs:
+        should_keep = filter_funcs[args.filter_func_name]
+    else:
+        print(f"No available function {args.filter_func_name}")
+        print(f"Available functions are:")
+        print(filter_funcs)
+        return
+
 
     dataset = ClassifierDatasetLoader([args.dataset_dir], use_gt_rope=False, load_true_states=True)
     filter_dataset(dataset_dir=args.dataset_dir,
                    dataset=dataset,
                    outdir=outdir,
-                   should_keep=_should_keep_example,
+                   should_keep=should_keep,
                    hparams_update=hparams_update,
                    do_not_process=False)
 
