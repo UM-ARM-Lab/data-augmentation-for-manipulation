e11f28edf041d378c872b8cc04cf7bc26e677351
Peter Mitrano <pmitrano@armstorm>
2021-03-24 10:56:36 -0400

    fix gripper orientation

--- link_bot_pycommon/src/link_bot_pycommon/dual_arm_rope_sim_val_with_robot_feasibility_checking_scenario.py
+++ link_bot_pycommon/src/link_bot_pycommon/dual_arm_rope_sim_val_with_robot_feasibility_checking_scenario.py
@@ -1,6 +1,7 @@
 from link_bot_pycommon.dual_arm_rope_with_robot_feasibility_checking_scenario import \
     DualArmRopeWithRobotFeasibilityCheckingScenario
 from link_bot_pycommon.dual_arm_sim_rope_scenario import SimValDualArmRopeScenario
+from tf.transformations import quaternion_from_euler
 
 
 class DualArmRopeSimValWithRobotFeasibilityCheckingScenario(DualArmRopeWithRobotFeasibilityCheckingScenario,
@@ -8,3 +9,6 @@ class DualArmRopeSimValWithRobotFeasibilityCheckingScenario(DualArmRopeWithRobot
     def __init__(self):
         SimValDualArmRopeScenario.__init__(self)
         DualArmRopeWithRobotFeasibilityCheckingScenario.__init__(self, self.robot_namespace)
+
+        self.left_preferred_tool_orientation = quaternion_from_euler(3.054, -0.851, 0.98)
+        self.right_preferred_tool_orientation = quaternion_from_euler(2.254, -0.747, 3.000)
