fc9f8cdc45c527f0ccb6e2f0fc63d3395068554d
Peter Mitrano <pmitrano@armstorm>
2021-03-26 18:41:01 -0400

    double num scenes

--- link_bot_planning/scripts/double_scenes.py
+++ link_bot_planning/scripts/double_scenes.py
@@ -2,6 +2,7 @@
 import argparse
 import logging
 import pathlib
+import shutil
 
 import colorama
 import tensorflow as tf
@@ -21,6 +22,20 @@ def main():
     for i in range(n_existing_scenes):
         j = i + n_existing_scenes
         print(f'{i}->{j}')
+
+        in_scene_file = args.dirname / f'scene_{i:04d}.bag'
+        out_scene_file = args.dirname / f'scene_{j:04d}.bag'
+
+        in_goal_file = args.dirname / f'goal_{i:04d}.pkl'
+        out_goal_file = args.dirname / f'goal_{j:04d}.pkl'
+
+        if out_scene_file.exists():
+            raise RuntimeError(f"file {out_scene_file.as_posix()} exists!")
+        if out_goal_file.exists():
+            raise RuntimeError(f"file {out_goal_file.as_posix()} exists!")
+
+        shutil.copy(in_scene_file, out_scene_file)
+        shutil.copy(in_goal_file, out_goal_file)
 
 
 if __name__ == '__main__':
