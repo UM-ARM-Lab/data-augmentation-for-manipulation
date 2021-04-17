8ee07111fe9e939a5ce417b7fa6c68e80e8c765e
Peter Mitrano <pmitrano@armstorm>
2021-04-17 13:43:25 -0400

    debugging the hang up

--- link_bot_pycommon/src/link_bot_pycommon/dual_arm_sim_rope_scenario.py
+++ link_bot_pycommon/src/link_bot_pycommon/dual_arm_sim_rope_scenario.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 import numpy as np
+from halo import Halo
 
 import ros_numpy
 import rosbag
@@ -167,6 +168,7 @@ class SimDualArmRopeScenario(BaseDualArmRopeScenario):
         self.service_provider.restore_from_bag(bagfile_name, excluded_models=[self.robot_name()])
         self.service_provider.play()
 
+    @Halo(text='Restoring', spinner='dots')
     def restore_from_bag(self, service_provider: BaseServices, params: Dict, bagfile_name):
         self.service_provider.play()
 
