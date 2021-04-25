097c4988070405f9893687f77d3379b285caaec8
Peter Mitrano <pmitrano@armstorm>
2021-04-25 10:32:02 -0400

    debugging

--- link_bot_planning/src/link_bot_planning/ompl_rrt_wrapper.py
+++ link_bot_planning/src/link_bot_planning/ompl_rrt_wrapper.py
@@ -122,6 +122,7 @@ class OmplRRTWrapper(MyPlanner):
         self.misc_rng.seed(seed)
         self.goal_sampler_rng.seed(seed)
         self.control_sampler_rng.seed(seed)
+        print(self.control_sampler_rng.get_state())
 
         self.last_propagate_time = time.perf_counter()
         self.progagate_dts = []
