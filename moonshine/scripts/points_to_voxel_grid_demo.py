bfa502307bafb5317f4ca69da7f4fb0bdd7f2950
Peter Mitrano <pmitrano@armstorm>
2021-05-18 11:24:28 -0400

    wow this is good

--- moonshine/scripts/points_to_voxel_grid_demo.py
+++ moonshine/scripts/points_to_voxel_grid_demo.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 import rospy
 from arc_utilities.ros_helpers import get_connected_publisher
+from moonshine.moonshine_utils import repeat_tensor
 from moonshine.raster_3d import points_to_voxel_grid
 from moonshine.simple_profiler import SimpleProfiler
 from rviz_voxelgrid_visuals.conversions import vox_to_voxelgrid_stamped
@@ -20,8 +21,10 @@ def perf():
     rng = tf.random.Generator.from_seed(0)
     batch_size = 32
     s = 64
-    n_points = 10000
-    batch_indices = rng.uniform([n_points], 0, batch_size, dtype=tf.int64)
+    n_points_per_vg = 10000
+    n_points = n_points_per_vg * batch_size
+    batch_indices = repeat_tensor(tf.range(batch_size, dtype=tf.int64), n_points_per_vg, 0, True)
+    batch_indices = tf.reshape(batch_indices, -1)
     points = rng.uniform([n_points, 3], dtype=tf.float32)
     res = tf.ones([n_points], dtype=tf.float32) * 0.01
     origin = tf.zeros([n_points, 3], dtype=tf.float32)
@@ -29,6 +32,15 @@ def perf():
     p.profile(100, points_to_voxel_grid, batch_indices, points, res, origin, h=s, w=s, c=s, batch_size=batch_size)
 
     print(p)
+
+    vg = points_to_voxel_grid(batch_indices, points, res, origin, h=s, w=s, c=s, batch_size=batch_size)
+    pub = get_connected_publisher('occupancy', VoxelgridStamped, queue_size=10)
+    for b in range(batch_size):
+        pub.publish(vox_to_voxelgrid_stamped(vg[b].numpy(),
+                                             scale=0.1,  # Each voxel is a 1cm cube
+                                             frame_id='world',  # In frame "world", same as rviz fixed frame
+                                             origin=origin.numpy()[b]))  # With origin centering the voxelgrid
+        input("press enter")
 
 
 def viz():
