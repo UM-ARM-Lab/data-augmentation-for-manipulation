7607de276ddb26b1bce2687149cd4f48afb335de
Peter Mitrano <pmitrano@armstorm>
2021-05-30 13:57:46 -0400

    make split dataset work for non tfrecords

--- link_bot_data/scripts/split_dataset.py
+++ link_bot_data/scripts/split_dataset.py
@@ -10,6 +10,7 @@ from link_bot_data.files_dataset import FilesDataset
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset_dir", type=pathlib.Path, help="directory of tfrecord files")
+    parser.add_argument("--extension", type=pathlib.Path, help="extension of the data files", default='tfrecords')
     parser.add_argument("--fraction-validation", '-v', type=float, help="fraction of files to put in validation",
                         default=DEFAULT_TEST_SPLIT)
     parser.add_argument("--fraction-testing", '-t', type=float, help="fraction of files to put in validation",
@@ -17,7 +18,7 @@ def main():
     args = parser.parse_args()
 
     files_dataset = FilesDataset(root_dir=args.dataset_dir)
-    sorted_records = sorted(list(args.dataset_dir.glob("*.tfrecords")))
+    sorted_records = sorted(list(args.dataset_dir.glob(f"example_*.{args.extension}")))
     for file in sorted_records:
         files_dataset.add(file)
     files_dataset.split()
