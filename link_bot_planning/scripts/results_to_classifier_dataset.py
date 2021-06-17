ddfc9cdd25f7f574dfc5c524fe6ca12c9f1cf852
Peter <peter@armnova>
2021-06-17 15:08:54 -0400

    generate classifier dataset from full tree of planning with heurstic classifier

--- link_bot_planning/scripts/results_to_classifier_dataset.py
+++ link_bot_planning/scripts/results_to_classifier_dataset.py
@@ -2,9 +2,6 @@
 import argparse
 import pathlib
 
-import colorama
-import numpy as np
-
 from arc_utilities import ros_init
 from link_bot_planning.results_to_classifier_dataset import ResultsToClassifierDataset
 from link_bot_pycommon.args import int_set_arg, BooleanOptionalAction
@@ -18,18 +15,24 @@ def main():
     parser.add_argument('--full-tree', action=BooleanOptionalAction)
     parser.add_argument("--labeling-params", type=pathlib.Path, help='labeling params')
     parser.add_argument("--visualize", action='store_true', help='visualize')
+    parser.add_argument("--regenerate", action='store_true', help='regenerate')
+    parser.add_argument("--only-rejected-transitions", action='store_true',
+                        help='only keep transitions the planner rejected')
     parser.add_argument("--trial-indices", type=int_set_arg, help='which plan(s) to show')
     parser.add_argument("--save-format", type=str, choices=['pkl', 'tfrecord'], default='pkl')
 
     args = parser.parse_args()
 
-    r = ResultsToClassifierDataset(args.results_dir,
-                                   args.outdir,
-                                   args.labeling_params,
-                                   args.trial_indices,
-                                   args.visualize,
-                                   args.save_format,
-                                   args.full_tree)
+    r = ResultsToClassifierDataset(results_dir=args.results_dir,
+                                   outdir=args.outdir,
+                                   labeling_params=args.labeling_params,
+                                   trial_indices=args.trial_indices,
+                                   visualize=args.visualize,
+                                   save_format=args.save_format,
+                                   full_tree=args.full_tree,
+                                   regenerate=args.regenerate,
+                                   only_rejected_transitions=args.only_rejected_transitions,
+                                   max_examples_per_trial=None)
 
     r.run()
 
--- link_bot_planning/src/link_bot_planning/results_to_classifier_dataset.py
+++ link_bot_planning/src/link_bot_planning/results_to_classifier_dataset.py
@@ -1,6 +1,5 @@
 import pathlib
 import tempfile
-from time import perf_counter
 from typing import Optional, List, Dict, Union
 
 import numpy as np
@@ -8,8 +7,9 @@ from colorama import Fore
 from progressbar import progressbar
 
 import rospy
+from arm_robots.robot import RobotPlanningError
 from link_bot_data.classifier_dataset_utils import add_perception_reliability, add_model_error_and_filter
-from link_bot_data.dataset_utils import tf_write_example, add_predicted, write_example
+from link_bot_data.dataset_utils import add_predicted, write_example
 from link_bot_data.split_dataset import split_dataset
 from link_bot_gazebo.gazebo_services import GazeboServices
 from link_bot_planning.analysis import results_utils
@@ -20,7 +20,7 @@ from link_bot_planning.my_planner import PlanningQuery, LoggingTree
 from link_bot_planning.test_scenes import get_states_to_save, save_test_scene_given_name
 from link_bot_pycommon.job_chunking import JobChunker
 from link_bot_pycommon.marker_index_generator import marker_index_generator
-from link_bot_pycommon.pycommon import deal_with_exceptions, try_make_dict_tf_float32
+from link_bot_pycommon.pycommon import deal_with_exceptions
 from link_bot_pycommon.serialization import my_hdump
 from moonshine.filepath_tools import load_hjson
 from moonshine.moonshine_utils import sequence_of_dicts_to_dict_of_tensors, add_batch_single, add_batch, remove_batch
@@ -41,10 +41,10 @@ class ResultsToClassifierDataset:
                  visualize: bool = False,
                  save_format: str = 'pkl',
                  full_tree: bool = False,
-                 test_split: Optional[float] = None,
-                 val_split: Optional[float] = None,
+                 regenerate: bool = False,
                  verbose: int = 1,
-                 subsample_fraction: Optional[float] = None,
+                 only_rejected_transitions: bool = False,
+                 max_examples_per_trial: Optional[int] = 500,
                  **kwargs):
         self.restart = False
         self.save_format = save_format
@@ -54,8 +54,10 @@ class ResultsToClassifierDataset:
         self.results_dir = results_dir
         self.outdir = outdir
         self.trial_indices = trial_indices
-        self.subsample_fraction = subsample_fraction
         self.verbose = verbose
+        self.regenerate = regenerate
+        self.only_rejected_transitions = only_rejected_transitions
+        self.max_examples_per_trial = max_examples_per_trial
 
         if labeling_params is None:
             labeling_params = pathlib.Path('labeling_params/classifier/dual.hjson')
@@ -79,6 +81,12 @@ class ResultsToClassifierDataset:
         outdir.mkdir(exist_ok=True, parents=True)
 
         self.gazebo_restarting_sub = rospy.Subscriber("gazebo_restarting", Empty, self.on_gazebo_restarting)
+
+        self.before_state_idx = marker_index_generator(0)
+        self.before_state_pred_idx = marker_index_generator(1)
+        self.after_state_idx = marker_index_generator(3)
+        self.after_state_pred_idx = marker_index_generator(4)
+        self.action_idx = marker_index_generator(5)
 
     def run(self):
         self.save_hparams()
@@ -139,23 +147,17 @@ class ResultsToClassifierDataset:
                 rospy.loginfo(f"Found existing classifier data for trial {trial_idx}")
                 continue
 
-            self.clear_markers()
-            self.before_state_idx = marker_index_generator(0)
-            self.before_state_pred_idx = marker_index_generator(1)
-            self.after_state_idx = marker_index_generator(3)
-            self.after_state_pred_idx = marker_index_generator(4)
-            self.action_idx = marker_index_generator(5)
+            self.reset_visualization()
 
             example_idx_for_trial = 0
 
             self.example_idx = compute_example_idx(trial_idx, example_idx_for_trial)
             try:
-                examples_gen = self.result_datum_to_dynamics_dataset(datum, trial_idx, self.subsample_fraction)
+                examples_gen = self.result_datum_to_dynamics_dataset(datum, trial_idx)
                 for example in progressbar(examples_gen):
                     self.example_idx = compute_example_idx(trial_idx, example_idx_for_trial)
                     total_examples += 1
-                    example = try_make_dict_tf_float32(example)
-                    full_filename = write_example(self.outdir, example, self.example_idx, self.save_format)
+                    write_example(self.outdir, example, self.example_idx, self.save_format)
                     example_idx_for_trial += 1
 
                     job_chunker.store_result(trial_idx, {'trial':              trial_idx,
@@ -169,46 +171,47 @@ class ResultsToClassifierDataset:
 
         print(Fore.LIGHTMAGENTA_EX + f"Wrote {total_examples} classifier examples" + Fore.RESET)
 
+    def reset_visualization(self):
+        self.clear_markers()
+        self.before_state_idx = marker_index_generator(0)
+        self.before_state_pred_idx = marker_index_generator(1)
+        self.after_state_idx = marker_index_generator(3)
+        self.after_state_pred_idx = marker_index_generator(4)
+        self.action_idx = marker_index_generator(5)
+
     def full_results_to_classifier_dataset(self):
         logfilename = self.outdir / 'logfile.hjson'
         job_chunker = JobChunker(logfilename)
 
-        t0 = perf_counter()
-        last_t = t0
-        max_examples_per_trial = 500
-        enough_trials_msg = f"moving on to next trial, already got {max_examples_per_trial} examples from this trial"
+        enough_trials_msg = f"moving to next trial, already got {self.max_examples_per_trial} examples from this trial"
         total_examples = 0
         for trial_idx, datum in results_utils.trials_generator(self.results_dir, self.trial_indices):
-            if job_chunker.has_result(str(trial_idx)):
+            if job_chunker.has_result(str(trial_idx)) and not self.regenerate:
                 rospy.loginfo(f"Found existing classifier data for trial {trial_idx}")
                 continue
 
             example_idx_for_trial = 0
 
-            self.example_idx = compute_example_idx(trial_idx, example_idx_for_trial)
-            for example in self.full_result_datum_to_dynamics_dataset(datum, trial_idx, self.subsample_fraction):
-                now = perf_counter()
-                dt = now - last_t
-                total_dt = now - t0
-                last_t = now
+            self.reset_visualization()
 
+            self.example_idx = compute_example_idx(trial_idx, example_idx_for_trial)
+            for example in self.full_result_datum_to_dynamics_dataset(datum, trial_idx):
                 self.example_idx = compute_example_idx(trial_idx, example_idx_for_trial)
                 total_examples += 1
-                example = try_make_dict_tf_float32(example)
-                tf_write_example(self.outdir, example, self.example_idx)
+                write_example(self.outdir, example, self.example_idx, self.save_format)
                 example_idx_for_trial += 1
 
                 if example_idx_for_trial > 50:
                     job_chunker.store_result(trial_idx, {'trial':              trial_idx,
                                                          'examples for trial': example_idx_for_trial})
-                if example_idx_for_trial > max_examples_per_trial:
+                if self.max_examples_per_trial is not None and example_idx_for_trial > self.max_examples_per_trial:
                     rospy.logwarn(enough_trials_msg)
                     break
 
             job_chunker.store_result(trial_idx, {'trial':              trial_idx,
                                                  'examples for trial': example_idx_for_trial})
 
-    def result_datum_to_dynamics_dataset(self, datum: Dict, trial_idx: int, subsample_fraction: float):
+    def result_datum_to_dynamics_dataset(self, datum: Dict, trial_idx: int):
         for t, transition in enumerate(get_transitions(datum)):
             environment, (before_state_pred, before_state), action, (after_state_pred, after_state), _ = transition
             if self.visualize:
@@ -230,7 +233,7 @@ class ResultsToClassifierDataset:
                 classifier_start_t=t,
             )
 
-    def full_result_datum_to_dynamics_dataset(self, datum: Dict, trial_idx: int, subsample_fraction: float):
+    def full_result_datum_to_dynamics_dataset(self, datum: Dict, trial_idx: int):
         steps = datum['steps']
         setup_info = datum['setup_info']
         planner_params = datum['planner_params']
@@ -241,14 +244,12 @@ class ResultsToClassifierDataset:
                 yield from self.dfs(planner_params,
                                     planning_query,
                                     planning_result.tree,
-                                    bagfile_name=setup_info.bagfile_name,
-                                    subsample_fraction=subsample_fraction)
+                                    bagfile_name=setup_info.bagfile_name)
 
     def dfs(self,
             planner_params: Dict,
             planning_query: PlanningQuery,
             tree: LoggingTree,
-            subsample_fraction: float,
             bagfile_name: Optional[pathlib.Path] = None,
             depth: int = 0,
             ):
@@ -260,27 +261,27 @@ class ResultsToClassifierDataset:
             bagfile_name = store_bagfile()
 
         for child in tree.children:
-            # uniformly randomly sub-sample? this is currently broken
-            # r = self.rng.uniform()
-            # if r < subsample_fraction:
-            skip_restore = len(tree.children) > 1 or depth == 0
             # if we only have one child we can skip the restore, this speeds things up a lot
-            if skip_restore:
+            must_restore = len(tree.children) > 1
+            if must_restore:
                 deal_with_exceptions('retry',
                                      lambda: self.scenario.restore_from_bag_rushed(
                                          service_provider=self.service_provider,
                                          params=planner_params,
                                          bagfile_name=bagfile_name))
-            before_state, after_state = self.execute(environment=planning_query.environment, action=child.action)
+            before_state, after_state, error = self.execute(environment=planning_query.environment, action=child.action)
+            if error:
+                continue
+
             if self.visualize:
                 self.visualize_example(action=child.action,
                                        after_state=after_state,
                                        before_state=before_state,
-                                       before_state_predicted={add_predicted(k): v for k, v in
-                                                               tree.state.items()},
-                                       after_state_predicted={add_predicted(k): v for k, v in child.state.items()},
+                                       before_state_predicted=tree.state,
+                                       after_state_predicted=child.state,
                                        environment=planning_query.environment)
 
+            # only include this example and continue the DFS if we were able to successfully execute the action
             yield from self.generate_example(
                 environment=planning_query.environment,
                 action=child.action,
@@ -291,7 +292,7 @@ class ResultsToClassifierDataset:
                 classifier_start_t=depth,
             )
             # recursion
-            yield from self.dfs(planner_params, planning_query, child, subsample_fraction, depth=depth + 1)
+            yield from self.dfs(planner_params, planning_query, child, depth=depth + 1)
 
     def generate_example(self,
                          environment: Dict,
@@ -301,6 +302,9 @@ class ResultsToClassifierDataset:
                          after_state: Dict,
                          after_state_pred: Dict,
                          classifier_start_t: int):
+        if self.only_rejected_transitions and after_state_pred['num_diverged'].squeeze() != 1:
+            return
+
         classifier_horizon = 2  # this script only handles this case
         example_states = sequence_of_dicts_to_dict_of_tensors([before_state, after_state])
         example_states_pred = sequence_of_dicts_to_dict_of_tensors([before_state_pred, after_state_pred])
@@ -336,18 +340,22 @@ class ResultsToClassifierDataset:
         if test_shape == 1:
             valid_out_example = remove_batch(valid_out_examples_batched)
 
-            yield valid_out_example
+            yield valid_out_example  # yield here is more convenient than returning example/None
         elif test_shape > 1:
             raise NotImplementedError()
         else:
-            pass  # do nothing if there are no examples, i.e. test_shape == 0
+            return  # do nothing if there are no examples, i.e. test_shape == 0
 
     def execute(self, environment: Dict, action: Dict):
         self.service_provider.play()
         before_state = self.scenario.get_state()
-        self.scenario.execute_action(environment=environment, state=before_state, action=action)
+        error = False
+        try:
+            self.scenario.execute_action(environment=environment, state=before_state, action=action)
+        except RobotPlanningError:
+            error = True
         after_state = self.scenario.get_state()
-        return before_state, after_state
+        return before_state, after_state, error
 
     def visualize_example(self,
                           action: Dict,
@@ -356,14 +364,23 @@ class ResultsToClassifierDataset:
                           after_state_predicted: Dict,
                           before_state_predicted: Dict,
                           environment: Dict):
+        before_state_predicted_viz = {add_predicted(k): v for k, v in before_state_predicted.items()}
+        before_state_predicted_viz['joint_names'] = before_state_predicted['joint_names']
+        after_state_predicted_viz = {add_predicted(k): v for k, v in after_state_predicted.items()}
+        after_state_predicted_viz['joint_names'] = after_state_predicted['joint_names']
+
         self.scenario.plot_environment_rviz(environment)
-        self.scenario.plot_state_rviz(before_state, idx=next(self.before_state_idx), label='actual')
-        self.scenario.plot_state_rviz(before_state_predicted, idx=next(self.before_state_pred_idx), label='predicted',
-                                      color='blue')
-        self.scenario.plot_state_rviz(after_state, idx=next(self.after_state_idx), label='actual')
-        self.scenario.plot_state_rviz(after_state_predicted, idx=next(self.after_state_pred_idx), label='predicted',
-                                      color='blue')
-        self.scenario.plot_action_rviz(before_state, action, idx=next(self.action_idx), label='actual')
+        self.scenario.plot_state_rviz(before_state, idx=next(self.before_state_idx), label='before_actual')
+        self.scenario.plot_state_rviz(before_state_predicted_viz, idx=next(self.before_state_pred_idx),
+                                      label='before_predicted', color='blue')
+        self.scenario.plot_state_rviz(after_state, idx=next(self.after_state_idx), label='after_actual')
+        self.scenario.plot_state_rviz(after_state_predicted_viz, idx=next(self.after_state_pred_idx),
+                                      label='after_predicted', color='blue')
+        self.scenario.plot_action_rviz(before_state, action, idx=next(self.action_idx), label='')
+        error = self.scenario.classifier_distance(after_state, after_state_predicted)
+        self.scenario.plot_error_rviz(error)
+        is_close = error < self.threshold
+        self.scenario.plot_is_close(is_close)
 
     def clear_markers(self):
         self.scenario.reset_planning_viz()
