d388b28e30b23d425018d61468099626ed4357b5
Peter <peter@armnova>
2021-10-22 17:11:02 -0400

    change defaults

--- link_bot_planning/scripts/iterative_fine_tuning.py
+++ link_bot_planning/scripts/iterative_fine_tuning.py
@@ -84,8 +84,8 @@ class IterativeFineTuning:
         self.initial_classifier_checkpoint = lpf('initial_classifier_checkpoint', default_classifier_checkpoint)
         self.initial_recovery_checkpoint = pathify(self.job_chunker.load_prompt('initial_recovery_checkpoint', None))
         planner_params_filename = lpf('planner_params_filename', 'planner_configs/val_car/random_accept.hjson')
-        self.test_scenes_dir = lpf('test_scenes_dir', 'test_scenes/swap_straps_no_recovery3')
-        self.test_scenes_indices = int_setify(self.job_chunker.load_prompt('test_scenes_indices', None))
+        self.test_scenes_dir = lpf('test_scenes_dir', 'test_scenes/car0-sm')
+        self.test_scenes_indices = int_setify(self.job_chunker.load_prompt('test_scenes_indices', 0))
 
         if not self.job_chunker.has_result('labeling_params_update'):
             ift_config = load_hjson(ift_config_filename)
