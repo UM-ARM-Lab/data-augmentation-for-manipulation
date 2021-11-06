2a952c5aa3ae59c17e14a28358bc253dcb7f1fb5
Peter <peter@armnova>
2021-11-06 00:57:41 -0400

    args

--- link_bot_data/scripts/cylinders.py
+++ link_bot_data/scripts/cylinders.py
@@ -12,12 +12,12 @@ from link_bot_data.merge_pkls import merge_pkls
 
 def _collect_dynamics_data(args):
     import sys
-    i, name, n_trajs_per = args
+    i, name, n_trajs_per, params = args
     sys.stderr = open(f'.log_{i}', 'w')
     # sys.stdout = sys.stdout
     with RosContext(f'collect_dynamics_data_{i}'):
         dataset_dir = collect_dynamics_data(
-            collect_dynamics_params=pathlib.Path('collect_dynamics_params/cylinders.hjson'),
+            collect_dynamics_params=params,
             seed=i,
             verbose=0,
             n_trajs=n_trajs_per,
@@ -30,12 +30,13 @@ def main():
     parser.add_argument('j', type=int, help='number of processes')
     parser.add_argument('n_trajs_total', type=int, help='number of trajs total')
     parser.add_argument('name', help='name of output dir')
+    parser.add_argument("params", type=pathlib.Path, default=pathlib.Path('collect_dynamics_params/cylinders.hjson'))
 
     args = parser.parse_args()
 
     with Pool(args.j) as p:
         trajs_splits = np.array_split(range(args.n_trajs_total), args.j)
-        process_args = [(i, args.name, len(trajs_split)) for i, trajs_split in enumerate(trajs_splits)]
+        process_args = [(i, args.name, len(trajs_split), args.hparams) for i, trajs_split in enumerate(trajs_splits)]
         dataset_dirs = list(p.imap_unordered(_collect_dynamics_data, process_args))
     outdir = pathlib.Path('fwd_model_data') / args.name
     merge_pkls(outdir, dataset_dirs, quiet=True)
