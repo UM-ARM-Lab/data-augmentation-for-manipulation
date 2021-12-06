6091098255c1dc684f9f710c745a122403a82f4a
Peter <peter@armnova>
2021-12-06 16:52:25 -0500

    fix ompl scenario for real val

--- link_bot_planning/src/link_bot_planning/get_ompl_scenario.py
+++ link_bot_planning/src/link_bot_planning/get_ompl_scenario.py
@@ -1,7 +1,7 @@
 from link_bot_planning.dual_arm_rope_ompl import DualArmRopeOmpl
 from link_bot_planning.floating_rope_ompl import FloatingRopeOmpl
 from link_bot_planning.rope_dragging_ompl import RopeDraggingOmpl
-from link_bot_pycommon.dual_arm_sim_rope_scenario import SimDualArmRopeScenario
+from link_bot_pycommon.base_dual_arm_rope_scenario import BaseDualArmRopeScenario
 from link_bot_pycommon.experiment_scenario import ExperimentScenario
 from link_bot_pycommon.floating_rope_scenario import FloatingRopeScenario
 from link_bot_pycommon.rope_dragging_scenario import RopeDraggingScenario
@@ -9,21 +9,12 @@ from link_bot_pycommon.scenario_ompl import ScenarioOmpl
 
 
 def get_ompl_scenario(scenario: ExperimentScenario, *args, **kwargs) -> ScenarioOmpl:
-    # order matters here, because of inheritence
+    # order matters here, because of inheritance
     if isinstance(scenario, RopeDraggingScenario):
         return RopeDraggingOmpl(scenario, *args, **kwargs)
-    elif isinstance(scenario, SimDualArmRopeScenario):
+    elif isinstance(scenario, BaseDualArmRopeScenario):
         return DualArmRopeOmpl(scenario, *args, **kwargs)
     elif isinstance(scenario, FloatingRopeScenario):
         return FloatingRopeOmpl(scenario, *args, **kwargs)
     else:
         raise NotImplementedError(f"unimplemented scenario {scenario}")
-
-
-# def get_planning_scenario(scenario: ExperimentScenario, *args, **kwargs) -> PlanningScenario:
-#     # order matters here, because of inheritence
-#     if isinstance(scenario, SimDualArmRopeScenario):
-#         return DualArmRopePlanning(scenario, *args, **kwargs)
-#     else:
-#         raise NotImplementedError(f"unimplemented scenario {scenario}")
-#
