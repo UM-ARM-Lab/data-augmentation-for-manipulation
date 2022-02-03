3ce6d17b4ed77e20c82f0b9722878236d7cbd885
Peter <peter@armnova>
2022-02-03 16:08:58 -0500

    add more variance when sampling augmentations

--- augmentation/src/augmentation/vae_augmentation.py
+++ augmentation/src/augmentation/vae_augmentation.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from augmentation.train_test_aug_vae import load_model_artifact, PROJECT
 from link_bot_pycommon.scenario_with_visualization import ScenarioWithVisualization
-from moonshine.vae import MyVAE
+from moonshine.vae import MyVAE, reparametrize
 
 
 class VAEAugmentation:
@@ -18,7 +18,16 @@ class VAEAugmentation:
         self.model.scenario = scenario
         self.scenario = scenario
         self.rng = np.random.RandomState(0)
+        self.temperature = 10
 
-    def aug_opt(self, example: Dict, batch_size: int, time: int):
-        example_aug = self.model(example)
+    def aug_opt(self, example: Dict, *args, **kwargs):
+        x = self.scenario.example_dict_to_flat_vector(example)
+
+        h = self.model.encoder(x)
+        mu = h[..., :int(self.model.hparams.latent_dim)]
+        log_var = h[..., int(self.model.hparams.latent_dim):]
+        hidden = reparametrize(mu, log_var / self.temperature)
+        x_reconstruction = self.model.decoder(hidden)
+
+        example_aug = self.scenario.flat_vector_to_example_dict(example, x_reconstruction)
         return example_aug
