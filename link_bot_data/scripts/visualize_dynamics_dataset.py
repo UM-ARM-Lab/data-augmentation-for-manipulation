79cb30415073a59d2c381a69b5e631945e9aee41
Peter <peter@armnova>
2022-03-28 16:16:03 -0400

    allow viz of specific examples

--- link_bot_data/scripts/visualize_dynamics_dataset.py
+++ link_bot_data/scripts/visualize_dynamics_dataset.py
@@ -4,8 +4,10 @@ import pathlib
 
 import numpy as np
 
+import rospy
 from arc_utilities import ros_init
 from link_bot_data.tf_dataset_utils import deserialize_scene_msg
+from link_bot_pycommon.args import int_set_arg
 from merrrt_visualization.rviz_animation_controller import RvizAnimationController
 from moonshine.numpify import numpify
 from state_space_dynamics.torch_dynamics_dataset import TorchDynamicsDataset
@@ -20,6 +22,7 @@ def main():
     parser.add_argument('--shard', type=int)
     parser.add_argument('--mode', choices=['train', 'test', 'val', 'all'], default='all', help='train test or val')
     parser.add_argument('--shuffle', action='store_true', help='shuffle')
+    parser.add_argument('--indices', type=int_set_arg, help='show specific example by example_idx value')
     parser.add_argument('--weight-above', type=float, default=0)
     parser.add_argument('--weight-below', type=float, default=1)
 
@@ -52,9 +55,11 @@ def main():
     while not dataset_anim.done:
         example_idx = dataset_anim.t()
         example = dataset[example_idx]
-        if example['example_idx'] not in [494, 495, 508, 529, 591, 596, 630, 684, 821, 948, 979]:
-            dataset_anim.step()
-            continue
+        if args.indices is not None:
+            rospy.logwarn_once(f"Got indices: {args.indices}")
+            if example['example_idx'] not in args.indices:
+                dataset_anim.step()
+                continue
         print(example['example_idx'])
         weight = example.get('weight', np.array([1]))
         total_weight += np.sum(weight)
