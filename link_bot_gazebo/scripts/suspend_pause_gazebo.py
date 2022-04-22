3a202f5bbb128e053fccfb48aa36a7c25c97ff61
Peter <peter@armnova>
2022-04-22 11:00:40 -0400

    run subparsers

--- link_bot_gazebo/scripts/suspend_pause_gazebo.py
+++ link_bot_gazebo/scripts/suspend_pause_gazebo.py
@@ -2,16 +2,19 @@
 import argparse
 
 from link_bot_gazebo.gazebo_utils import get_gazebo_processes
+from link_bot_pycommon.args import run_subparsers
 
 
 def suspend(args):
     gazebo_processes = get_gazebo_processes()
     [p.suspend() for p in gazebo_processes]
+    print(gazebo_processes)
 
 
 def resume(args):
     gazebo_processes = get_gazebo_processes()
     [p.resume() for p in gazebo_processes]
+    print(gazebo_processes)
 
 
 def main():
@@ -21,6 +24,8 @@ def main():
     suspend_parser.set_defaults(func=suspend)
     resume_parser = subparsers.add_parser('resume')
     resume_parser.set_defaults(func=resume)
+
+    run_subparsers(parser)
 
 
 if __name__ == '__main__':
