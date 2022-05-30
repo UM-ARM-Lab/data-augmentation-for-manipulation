fb1cc7dc7036f3b1af5877cfc1f9131bb6729dc4
Peter <peter@armnova>
2022-05-30 11:07:03 -0400

    deletes/cleanups

--- state_space_dynamics/src/state_space_dynamics/filter_utils.py
+++ state_space_dynamics/src/state_space_dynamics/filter_utils.py
@@ -1,18 +1,13 @@
 import pathlib
 from typing import List
 
-from link_bot_pycommon.experiment_scenario import ExperimentScenario
-from link_bot_pycommon.get_scenario import get_scenario
 from moonshine.filepath_tools import load_trial
 from state_space_dynamics.base_filter_function import BaseFilterFunction, PassThroughFilter
 
 
-def load_filter(model_dirs: List[pathlib.Path], scenario: ExperimentScenario = None) -> BaseFilterFunction:
+def load_filter(model_dirs: List[pathlib.Path], _) -> BaseFilterFunction:
     representative_model_dir = model_dirs[0]
     _, common_hparams = load_trial(representative_model_dir.parent.absolute())
-    if scenario is None:
-        scenario_name = common_hparams['dynamics_dataset_hparams']['scenario']
-        scenario = get_scenario(scenario_name)
     model_type = common_hparams['model_class']
     if model_type in ['none', 'pass-through']:
         return PassThroughFilter()
