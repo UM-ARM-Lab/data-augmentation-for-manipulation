1b41f933ff4c3fd707933acd0e4002272c4c798d
Peter <peter@armnova>
2022-06-05 09:03:18 -0400

    change mask to reflect best results from search

--- state_space_dynamics/scripts/search_threshold.py
+++ state_space_dynamics/scripts/search_threshold.py
@@ -7,6 +7,7 @@ import hjson
 from tqdm import tqdm
 
 from arc_utilities import ros_init
+from link_bot_pycommon.job_chunking import JobChunker
 from moonshine.filepath_tools import load_hjson
 from moonshine.magic import wandb_lightning_magic
 from state_space_dynamics import train_test_dynamics
@@ -32,8 +33,14 @@ def main():
         0.5,
     ]
 
+    root = pathlib.Path("results")
+    root.mkdir(exist_ok=True)
+    logfile_name = root / 'search_threshold.hjson'
+    job_chunker = JobChunker(logfile_name)
+
     for threshold in tqdm(thresholds):
-        test_threshold(args.dataset_dir, threshold, batch_size=64, repeat=5)
+        sub_chunker = job_chunker.sub_chunker(key=f'{threshold}')
+        test_threshold(sub_chunker, args.dataset_dir, threshold, batch_size=64, repeat=5)
 
 
 def update_params(params_filename, update_dict):
@@ -44,41 +51,59 @@ def update_params(params_filename, update_dict):
     return pathlib.Path(tmp_params_file.name)
 
 
-def test_threshold(dataset_dir, threshold: float, batch_size: int, repeat: int):
+def test_threshold(job_chunker: JobChunker, dataset_dir, threshold: float, batch_size: int, repeat: int):
     tmp_iterative_lowest_error_params_filename = update_params(pathlib.Path("hparams/iterative_lowest_error.hjson"),
                                                                {'mask_threshold': threshold})
-    test_threshold_w_params(dataset_dir, tmp_iterative_lowest_error_params_filename,
+    test_threshold_w_params(job_chunker.sub_chunker("iterative_lowest_error"),
+                            dataset_dir, tmp_iterative_lowest_error_params_filename,
                             f'search_iterative_lowset_error_{threshold}', batch_size, repeat)
 
     tmp_low_initial_error_params_filename = update_params(pathlib.Path("hparams/low_initial_error.hjson"),
                                                           {'mask_threshold': threshold})
-    test_threshold_w_params(dataset_dir, tmp_low_initial_error_params_filename,
+    test_threshold_w_params(job_chunker.sub_chunker("low_initial_error"),
+                            dataset_dir, tmp_low_initial_error_params_filename,
                             f'search_low_initial_error_{threshold}', batch_size, repeat)
 
 
-def test_threshold_w_params(dataset_dir: pathlib.Path,
+def test_threshold_w_params(job_chunker: JobChunker,
+                            dataset_dir: pathlib.Path,
                             params_filename: pathlib.Path,
                             nickname: str,
                             batch_size: int,
                             repeat: int):
     unadapted_checkpoint = 'unadapted-b8s5s'
 
-    fine_tuned_checkpoint = train_test_dynamics.fine_tune_main(dataset_dir=dataset_dir,
-                                                               checkpoint=unadapted_checkpoint,
-                                                               params_filename=params_filename,
-                                                               batch_size=batch_size,
-                                                               epochs=-1,
-                                                               nickname=nickname,
-                                                               steps=500_000,
-                                                               user='armlab',
-                                                               seed=0,
-                                                               repeat=repeat)
-    test_dataset_dir = pathlib.Path("known_good_4")
-    train_test_dynamics.eval_main(dataset_dir=test_dataset_dir,
-                                  checkpoint=fine_tuned_checkpoint,
-                                  mode='test',
-                                  batch_size=batch_size,
-                                  user='armlab')
+    fine_tuned_checkpoint = job_chunker.get("fine_tuned_checkpoint")
+    if fine_tuned_checkpoint is None:
+        fine_tuned_checkpoint = train_test_dynamics.fine_tune_main(dataset_dir=dataset_dir,
+                                                                   checkpoint=unadapted_checkpoint,
+                                                                   params_filename=params_filename,
+                                                                   batch_size=batch_size,
+                                                                   epochs=-1,
+                                                                   nickname=nickname,
+                                                                   steps=500_000,
+                                                                   user='armlab',
+                                                                   seed=0,
+                                                                   repeat=repeat)
+        job_chunker.store_result("fine_tuned_checkpoint", fine_tuned_checkpoint)
+
+    if not job_chunker.has_result("known_good_4_eval"):
+        test_dataset_dir = pathlib.Path("known_good_4")
+        metrics = train_test_dynamics.eval_main(dataset_dir=test_dataset_dir,
+                                                checkpoint=fine_tuned_checkpoint,
+                                                mode='test',
+                                                batch_size=batch_size,
+                                                user='armlab')
+        job_chunker.store_result("known_good_4_eval", metrics[0]['test_loss'])
+
+    if not job_chunker.has_result("long_eval"):
+        test_dataset_dir = pathlib.Path("no_car_alt_long_1_1654188361_f64781fe34_32")
+        metrics = train_test_dynamics.eval_main(dataset_dir=test_dataset_dir,
+                                                checkpoint=fine_tuned_checkpoint,
+                                                mode='test',
+                                                batch_size=batch_size,
+                                                user='armlab')
+        job_chunker.store_result("long_eval", metrics[0]['test_loss'])
 
 
 if __name__ == '__main__':
